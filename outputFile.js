b7332750a3f7f0a202e0eea55be79e5fda10bb30
Chanel <rayfieldchanel@gmail.com>
2018-02-15 20:58:21 -0600

    first commit

---
+++ outputFile.js
@@ -0,0 +1,11 @@
+function consolePrint () {
+    console.log("Hello Chanel");
+
+}
+    function alertPrint() {
+        alert("Hello Chanel");
+    }
+
+    function documentPrint(){
+        document.write("Hello Chanel")
+    }
